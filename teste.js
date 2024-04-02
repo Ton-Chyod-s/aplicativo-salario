14d5e5dd31dd1682582b2940f1f5560eb9fb9a7b
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-04-02 17:28:56 -0400

    doc: att

--- teste.js
+++ teste.js
@@ -1,8 +1,7 @@
 
 const salario =  async (value) => {
     const result = await fetch(`https://api-salario.cyclic.app/salario/${value}`).then((res) => res.json().then((data) => {
-        console.log(data)
-        return data
+        console.log(data.Despesas)
     }))
     return result
 }
