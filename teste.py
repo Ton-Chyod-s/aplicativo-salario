511b664789f6dcaac97733774c7c8a86a41bdb1f
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-04-05 14:06:21 -0400

    doc: att

--- teste.py
+++
@@ -1,6 +0,0 @@
-import requests
-from datetime import datetime
-
-requisicao = requests.get("https://api-salario.cyclic.app/salario/2640")
-requisicao_dict = requisicao.json()
-print(requisicao_dict)
